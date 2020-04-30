b238c2385baf18d72c14555e88a81dc993306d98
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-04-30 17:23:36 +0530

    finished up blog routing

--- src/app/blog/blog-details/blog-details.component.ts
+++ src/app/blog/blog-details/blog-details.component.ts
@@ -8,15 +8,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./blog-details.component.sass'],
 })
 export class BlogDetailsComponent implements OnInit {
+  name: string;
+  blog: any;
   constructor(private blogService: BlogService, private route: ActivatedRoute) {
     this.route.params.subscribe((params: any) => {
-      console.log(params)
+      this.name = params.name;
     })
    }
 
   ngOnInit() {
-    this.blogService.getBlog('README').subscribe((data: any) => {
-      console.log(atob(data.content))
+    this.blogService.getBlog(this.name).subscribe((data: any) => {
+      const blog = atob(data.content);
+      this.blog = this.blogService.convertMd(blog);
     })
   }
 
